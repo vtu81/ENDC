6ae6a6f4f9246fcc6d30ac2477b37a18c7613c45
vtu81 <48283858+vtu81@users.noreply.github.com>
2021-12-13 20:23:08 +0800

    FashionMNIST experiments added

---
+++ datasets/downloader.py
@@ -0,0 +1,11 @@
+from torchvision import datasets
+
+def main():
+    data_path = './'
+    datasets.MNIST(root=data_path, train=True, download=True)
+    datasets.MNIST(root=data_path, train=False, download=True)
+    datasets.FashionMNIST(root=data_path, train=True, download=True)
+    datasets.FashionMNIST(root=data_path, train=False, download=True)
+
+if __name__ == "__main__":
+    main()
